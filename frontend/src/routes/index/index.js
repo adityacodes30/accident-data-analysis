6084373596011b5c6aa5b419619b4baef4e138c5
Mankirat Singh <mankiratsingh1315@gmail.com>
2024-04-14 23:31:22 +0530

    landing page update

--- frontend/src/routes/index/index.js
+++ frontend/src/routes/index/index.js
@@ -3,14 +3,20 @@ import { Link } from "react-router-dom";
 export default function IndexPage() {
   return (
     <div>
-      <h1>This is the index page</h1>
+      {/* <h1>This is the index page</h1> */}
+      <div style={{ backgroundColor: 'black', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+        <h1 style={{ color: 'white', fontSize: '4rem', marginBottom: '2rem' }}>Accident Data Analysis</h1>
+        <div style={{ display: 'flex', justifyContent: 'space-between', width: '20%' }}>
+          <Link to="/sign-up" style={{ backgroundColor: 'white', padding: '1rem 2rem', borderRadius: '5px', border: 'none', cursor: 'pointer' }}>Login</Link>
+          <Link to="/sign-in" style={{ backgroundColor: 'white', padding: '1rem 2rem', borderRadius: '5px', border: 'none', cursor: 'pointer' }}>Signup</Link>
+        </div>
+      </div>
       <div>
-        <ul>
+        {/* <ul>
           <li><Link to="/sign-up">Sign Up</Link></li>
           <li><Link to="/sign-in">Sign In</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
           <li><Link to="/dashboard">Dashboard</Link></li>
-        </ul>
+        </ul> */}
       </div>
     </div>
   )
